9bf7522d25676a951a9dbacfe1c5449614ffa747
I797GMIGUE22 <miguelorjuela20172017outlook.com>
2022-10-18 09:12:41 -0500

    Se eliminaron los comentarios de clase

--- js/codigo.js
+++ js/codigo.js
@@ -1,9 +1,3 @@
-//Declarar una funcion como si fuera una variable (hacer una nueva funcion) con function sin (=) xd
-// Si tienes que copiar y pegar lo mismo eso merece ser una funcion
-// funcion en minuscula y parametros entre ()
-// Primero se declaran y luego se usan las funciones
-//IMPORTANTE return es una instruccion que sirve para que la variable se defina porque manda la informacion hacia la nueva variable o que una funcion ejecute cierta funcion que se especifica en su interior
-
 function aleatorio (min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }         
@@ -36,12 +30,6 @@ function ringring(o){
     return a = alert(election(jugador) + o + electionpc(pc))
 }
 
-// Algoritmo de aleatoriedad de numeros entre los rangos (1 - 3)
-// 1 es piedra, 2 es papel y 3 es tijera
-// Doctype es compatibilidad con navegadores viejos
-// Las instrucciones son terminos que modifican el flujo del codigo
-// while = haga un ciclo mientras sea verdadera y importante
-                            
 let jugador = 0
 let pc = 0
 let triunfos = 0
@@ -50,18 +38,6 @@ let perdidas = 0
 while (triunfos <3 && perdidas < 3){
     pc = aleatorio (1, 3)
     jugador = prompt("Elige: 1 = 🤘 | 2 = 🧻 | 3 = ✂️")
-//alert("Elegiste " + jugador)
-// (==) = Comparar
-// (=) = Asignacion
-// if = si
-// else = si no 
-// Comentario de desarollador
-// else if = si no, entonces
-//COMBATE//
-//&& = y, para que dos cosas se comparen o hagan al mismo tiempo
-// Math.floor = funcion que quita decimales a cualquier numero 
-// Math.random () = Da un numero aleatorio entre en 0 y el 1
-
     if(pc == jugador){
         ringring(o ="--EMPATE--")
     } else if((jugador == 1 && pc == 3) || (jugador == 2 && pc == 1) || (jugador == 3 && pc == 2)){
